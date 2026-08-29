891049cd3f0bcda246587c7922542fad89bd0127
Giorgos Tsompanakis <g.tsobanakis@gmail.com>
2026-08-29 11:08:37 +0300

    chore: initialize Express application

---
+++ src/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        status: 'running'
+    })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Secure OTP Lab running on http://localhost:${PORT}`)
+})
